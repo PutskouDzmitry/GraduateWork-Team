6e160d469c65a024b609172e57cf50c7b716f40a
Dzmitry_Putskou <dimaputkovvv@gmail.com>
2022-03-25 14:57:31 +0300

    Update project

--- server/pkg/api/wifiApi.go
+++ server/pkg/api/wifiApi.go
@@ -1,46 +1,52 @@
 package api
 
 import (
-	"fmt"
 	"github.com/PutskouDzmitry/GraduateWork-Team/server/pkg/model"
 	"github.com/PutskouDzmitry/GraduateWork-Team/server/pkg/service"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
+type T struct {
+	file string `json:"file"`
+}
+
 func (h Handler) calculationOfValues(c *gin.Context) {
 	var routers []model.RouterSettings
-	if err := c.BindJSON(&routers); err != nil {
+	var t T
+	if err := c.BindJSON(&t); err != nil {
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
 	logrus.Info("start data from front-end ", routers)
 
-	file, header, err := c.Request.FormFile("file")
-	logrus.Info(file)
-	filename := header.Filename
-	fmt.Println(header.Filename)
-	out, err := os.Create(filename + ".png")
+	response, e := http.Get(t.file)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer response.Body.Close()
+	out, err := os.Create("filename" + ".png")
 	if err != nil {
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
 	defer out.Close()
-	_, err = io.Copy(out, file)
+	_, err = io.Copy(out, response.Body)
 	if err != nil {
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	draw := service.NewDrawImage(routers, filename)
+	draw := service.NewDrawImage(routers, "filename"+".png")
 	err = draw.DrawOnImage()
 	if err != nil {
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	filepath := "http://localhost:8080/file/" + filename
+	filepath := "http://localhost:8080/file/" + "qwe"
 	c.JSON(http.StatusOK, gin.H{"filepath": filepath})
 }
 
