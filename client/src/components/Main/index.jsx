0fd3cd764673b58fc4b6168e33dd109b9d0ee7db
FinnYguY <tema.menschikoff2013165@gmail.com>
2022-04-19 18:44:36 +0300

    pisun

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -8,29 +8,30 @@ const fileTypes = ["JPG", "JPEG", "PNG", "GIF"];
 
 function Main() {
   const canvas = useRef(null);
+  const form = useRef(null);
   const [isUploaded, setIsUploaded] = useState(false);
   const [routers, setRouters] = useState([]);
-  const [formData, setFormData] = useState(new FormData());
+  // const [formData, setFormData] = useState(new FormData());
 
-  const handleChange = async (file) => {
-    canvas.current.getContext("2d").clearRect(0, 0, 600, 400);
-    let ctx = canvas.current.getContext("2d");
-    let url = URL.createObjectURL(file[0]);
-    let img = new Image();
-    img.onload = function () {
-      ctx.drawImage(img, 0, 0);
-    };
-    img.src = url;
+  // const handleChange = async (file) => {
+  //   canvas.current.getContext("2d").clearRect(0, 0, 600, 400);
+  //   let ctx = canvas.current.getContext("2d");
+  //   let url = URL.createObjectURL(file[0]);
+  //   let img = new Image();
+  //   img.onload = function () {
+  //     ctx.drawImage(img, 0, 0);
+  //   };
+  //   img.src = url;
 
-    let imageBlob = await new Promise((resolve) =>
-      canvas.current.toBlob(resolve, "image/png")
-    );
-    let formData = new FormData();
-    formData.append("file", imageBlob, file[0].name);
-    console.log(formData);
-    setIsUploaded(true);
-    setFormData(formData);
-  };
+  //   let imageBlob = await new Promise((resolve) =>
+  //     canvas.current.toBlob(resolve, "image/png")
+  //   );
+  //   let formData = new FormData();
+  //   formData.append("file", imageBlob, file[0].name);
+  //   console.log(formData);
+  //   setIsUploaded(true);
+  //   setFormData(formData);
+  // };
 
   useEffect(() => {
     const currentCanvas = canvas.current;
@@ -49,6 +50,21 @@ function Main() {
       currentCanvas.removeEventListener("click", clickListener);
     };
   });
+
+  const handleForm = async (form) => {
+    // let imageBlob = await new Promise((resolve) =>
+    //   canvas.current.toBlob(resolve, "image/png")
+    // );
+    // let formData = new FormData();
+    // formData.append("image", imageBlob, "image.png");
+
+    // const picture = await fetch("http://localhost:8080/api/wifi/kek", {
+    //   method: "POST",
+    //   body: formData,
+    // }).then((response) => response.json());
+    // console.log(picture);
+    console.log(form);
+  };
 
   return (
     <div className="main-block">
@@ -74,13 +90,23 @@ function Main() {
           );
         })}
       </div>
-      <FileUploader
+      {/* <FileUploader
         multiple={true}
         handleChange={handleChange}
         name="file"
         types={fileTypes}
-      />
-      <button
+      /> */}
+      <form
+        ref={form}
+        action="http://localhost:8080/api/unit1/calculation"
+        encType="multipart/form-data"
+        method="POST"
+        // onSubmit={() => handleForm(form.current)}
+      >
+        <input type="file" name="file" accept="*" />
+        <button type="submit">submit</button>
+      </form>
+      {/* <button
         onClick={async () => {
           const picture = await fetch("http://localhost:8080/api/wifi/kek", {
             method: "POST",
@@ -90,7 +116,7 @@ function Main() {
         }}
       >
         Kek
-      </button>
+      </button> */}
     </div>
   );
 }
