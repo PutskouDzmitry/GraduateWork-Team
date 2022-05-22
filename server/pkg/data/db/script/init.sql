bc733f8710cbfac9eeb31d1dbc7599c1f40845a4
Dzmitry_Putskou <dimaputkovvv@gmail.com>
2022-05-22 11:51:23 +0300

    update

--- server/pkg/data/db/script/init.sql
+++ server/pkg/data/db/script/init.sql
@@ -7,14 +7,14 @@ CREATE TABLE "wifi_data_models"
 
 CREATE TABLE "users"
 (
-    id serial not null unique,
+    id SERIAL NOT NULL UNIQUE,
     username CHAR(50) NOT NULL,
     password CHAR(100) NOT NULL
 );
 
 CREATE TABLE "router_data_models"
 (
-  id_router serial not null unique,
+  id_router SERIAL NOT NULL UNIQUE,
   coordinates_of_router_id int not null,
   transmitter_power float,
   gain_of_transmitting_antenna float,
@@ -24,7 +24,6 @@ CREATE TABLE "router_data_models"
   signal_loss_receiving float,
   number_of_channels float,
   scale float,
-  thickness float,
   com float
 );
 
